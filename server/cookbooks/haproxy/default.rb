3d91d17cb91aba2f25d77c3da76be8da591ef57d
Sorah Fukumori <sorah@cookpad.com>
2018-07-11 10:18:04 +0900

    rproxy-s3 (s3-apne1.ioi18.net)

---
+++ server/cookbooks/haproxy/default.rb
@@ -0,0 +1,8 @@
+package 'haproxy'
+
+directory "/etc/haproxy" do
+  owner 'root'
+  group 'root'
+  mode  '0755'
+end
+
---
+++ server/roles/rproxy-s3/default.rb
@@ -0,0 +1,25 @@
+include_role 'base'
+include_cookbook 'nginx'
+include_cookbook 'haproxy'
+
+template '/etc/haproxy/haproxy.cfg' do
+  owner 'root'
+  group 'root'
+  mode  '0644'
+  notifies :reload, 'service[haproxy]'
+end
+
+service 'haproxy' do
+  action [:enable, :start]
+end
+
+template '/etc/nginx/conf.d/default.conf' do
+  owner 'root'
+  group 'root'
+  mode  '0644'
+  notifies :run, 'execute[nginx try-reload]'
+end
+
+service  'nginx' do
+  action [:enable, :start]
+end
