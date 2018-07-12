abf388e3561e13699d5aeff39f9543fa9c887609
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-07-12 01:31:38 +0000

    apps for contestant machines

--- itamae/roles/contestant/default.rb
+++ itamae/roles/contestant/default.rb
@@ -14,3 +14,33 @@ end
 package 'ubuntu-desktop'
 
 include_cookbook 'compilers'
+
+## Editors
+package 'emacs'
+package 'gedit'
+package 'nano'
+package 'vim'
+package 'vim-gtk3'
+
+## Interpreters
+package 'python'
+package 'python-doc'
+package 'python3'
+package 'python3-doc'
+package 'ruby'
+package 'ruby2.5-doc'
+
+## Debuggers
+package 'ddd'
+package 'ddd-doc'
+package 'gdb'
+package 'gdb-doc'
+package 'valgrind'
+package 'visualvm'
+
+## Utilities
+package 'firefox'
+package 'gnome-terminal'
+
+package 'byobu'
+package 'screen'
