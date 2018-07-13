cbe7c3d224a16aa5b76b9a925d8b61b48008ecad
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-07-13 04:25:31 +0000

    image: clear machine-ids before packaging

--- image/scripts/cleanup.sh
+++ image/scripts/cleanup.sh
@@ -1,4 +1,4 @@
-rm -rf /tmp/itamae
+systemd-tmpfiles --remove
 
 apt-get autoremove --purge
 apt-get clean
@@ -7,3 +7,7 @@ find /var/lib/apt/lists -type f -delete
 sed -e '/http::proxy/Id' -i /etc/apt/apt.conf
 
 usermod -L provisioner
+
+for idfile in /etc/machine-id /var/lib/dbus/machine-id; do
+    test -f "$idfile" && truncate -s 0 "$idfile"
+done
