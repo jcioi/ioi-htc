b638931ff520f627408b357fa71b0c7e34d45e79
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-07-18 00:26:57 +0000

    xdg-desktop

--- itamae/roles/contestant/desktop.rb
+++ itamae/roles/contestant/desktop.rb
@@ -1,34 +1,4 @@
-directory '/usr/local/share/applications' do
-  owner 'root'
-  group 'root'
-  mode '755'
-end
-
-define :desktop_entry, display_name: nil, comment: nil, icon: nil, exec: nil, terminal: false, categories: [] do
-  display_name = params[:display_name] || fail
-  comment = params[:comment] || display_name
-  icon = params[:icon] || fail
-  exec = params[:exec] || fail
-  terminal = !!params[:terminal]
-  categories = [*params[:categories]].map {|s| "#{s};" }.join
-
-  file "/usr/local/share/applications/#{params.fetch(:name)}.desktop" do
-    owner 'root'
-    group 'root'
-    mode '644'
-    content <<EOF
-[Desktop Entry]
-Type=Application
-Name=#{display_name}
-Comment=#{comment}
-Icon=#{icon}
-Exec=#{exec}
-Terminal=#{terminal}
-Categories=#{categories}
-EOF
-  end
-end
-
+include_cookbook 'xdg-desktop'
 
 desktop_entry 'cppreference' do
   display_name 'C++/C Reference'
