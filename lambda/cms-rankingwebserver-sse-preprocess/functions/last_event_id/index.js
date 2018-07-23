5388c1c368c64da5070dbb5d691b32373419a902
Sorah Fukumori <sorah@cookpad.com>
2018-07-24 06:27:04 +0900

    prefer last-event-id in header, over querystring

--- lambda/cms-rankingwebserver-sse-preprocess/functions/last_event_id/index.js
+++ lambda/cms-rankingwebserver-sse-preprocess/functions/last_event_id/index.js
@@ -14,12 +14,17 @@ exports.handle = (event, context, callback) => {
   console.log(`querystring: ${request.querystring}`);
   if (lastEventIdQueryMatch) {
     console.log(`match: ${lastEventIdQueryMatch}`);
-    request.headers['last-event-id'] = [{
-      'key': 'last-event-id',
-      'value': lastEventIdQueryMatch[2],
-    }];
-    request.querystring = request.querystring.replace(/([;&]|^)last_event_id=(.+?)([;&]|$)/g, '$1$3').replace(/;;|&&/g, '');
+    if (request.headers['last-event-id']) {
+      console.log(`last-event-id header exists`);
+    } else {
+      console.log(`last-event-id set to ${lastEventIdQueryMatch[2]}`);
+      request.headers['last-event-id'] = [{
+        'key': 'last-event-id',
+        'value': lastEventIdQueryMatch[2],
+      }];
+    }
     console.log(`headers: ${JSON.stringify(request.headers)}`);
+    request.querystring = request.querystring.replace(/([;&]|^)last_event_id=(.+?)([;&]|$)/g, '$1$3').replace(/;;|&&/g, '');
     console.log(`querystring: ${request.querystring}`);
   }
 
