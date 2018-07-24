2605dfb46729a21539dda65cb2bfa197dc9450b6
Sorah Fukumori <sorah@cookpad.com>
2018-07-24 10:46:42 +0900

    cms-proxyservice: missing resourceservice

--- itamae/roles/cms-proxyservice/default.rb
+++ itamae/roles/cms-proxyservice/default.rb
@@ -2,6 +2,10 @@ include_role 'base'
 include_cookbook 'cms'
 include_cookbook 'codedeploy-agent'
 
+service 'cms-resourceservice.service' do
+  action [:enable]
+end
+
 service 'cms-proxyservice.service' do
   action [:enable]
 end
