29a2e6616b7959e9fc2c55f64d559f88bd1a7639
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-07-26 17:47:49 +0000

    contestant: separate recipe for apps

--- itamae/roles/contestant/default.rb
+++ itamae/roles/contestant/default.rb
@@ -16,62 +16,11 @@ node[:contestant][:cms_uri] ||=
 
 include_cookbook 'compilers'
 
-## Base Desktop Environment
 package 'ubuntu-desktop'
 package 'fonts-noto'
 package 'fonts-noto-cjk'
 
-## Editors
-package 'codeblocks'
-package 'codeblocks-contrib'
-package 'hunspell'  # for codeblocks (spellchecker plugin)
-package 'geany'
-package 'geany-plugins'
-package 'emacs'
-package 'gedit'
-package 'joe'
-package 'kate'
-package 'kdevelop'
-package 'lazarus'
-package 'fpc-source'  # for lazarus
-package 'nano'
-package 'vim'
-package 'vim-gtk3'
-
-include_recipe './eclipse.rb'
-include_recipe './intellij_idea.rb'
-
-## Interpreters
-package 'python'
-package 'python-doc'
-package 'python3'
-package 'python3-doc'
-package 'ruby'
-package 'ruby2.5-doc'
-
-## Debuggers
-package 'cppcheck'
-package 'ddd'
-package 'ddd-doc'
-package 'gdb'
-package 'gdb-doc'
-package 'valgrind'
-package 'visualvm'
-
-## Utilities
-package 'firefox'
-package 'gnome-terminal'
-package 'konsole'
-package 'xterm'
-
-package 'byobu'
-package 'mc'
-package 'screen'
-
-## Extra documents
-package 'cppreference-doc-en-html'
-
-
+include_recipe './apps.rb'
 include_recipe './desktop.rb'
 include_recipe './skel.rb'
 include_recipe './trust-launchers.rb'
