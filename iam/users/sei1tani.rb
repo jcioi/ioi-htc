e0b0e3787bcdac5a1412e9f38c24184e73fcd0f7
Sorah Fukumori <sorah@cookpad.com>
2018-07-28 05:03:53 +0900

    password_reset_required=>false

--- iam/users/sei1tani.rb
+++ iam/users/sei1tani.rb
@@ -1,5 +1,5 @@
 user "sei1tani", :path=>"/" do
-  login_profile :password_reset_required=>true
+  login_profile :password_reset_required=>false
 
   attached_managed_policies(
     "arn:aws:iam::aws:policy/AdministratorAccess",
