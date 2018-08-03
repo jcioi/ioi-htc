a00e42ba43b8acfbd6afab511da4b3effcfa34aa
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-08-03 22:15:58 +0900

    iam: ECS task roles for translation

---
+++ iam/roles/EcsTranslation.rb
@@ -0,0 +1,38 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+role "EcsTranslation", :path=>"/" do
+  max_session_duration 43200
+
+  assume_role_policy_document do
+    {"Version"=>"2012-10-17",
+     "Statement"=>
+       [{"Effect"=>"Allow",
+         "Principal"=>{"Service"=>"ecs-tasks.amazonaws.com"},
+         "Action"=>"sts:AssumeRole"}]}
+  end
+
+  policy "translation-s3" do
+    {
+      "Version" => "2012-10-17",
+      "Statement" => [
+        {
+          "Effect" => "Allow",
+          "Action" => %w(
+            s3:GetObject
+            s3:PutObject
+            s3:DeleteObject
+            s3:ListBucket
+          ),
+          "Resource" => %w(
+            arn:aws:s3:::ioi18-translation-files-prd
+            arn:aws:s3:::ioi18-translation-files-prd/*
+          ),
+        },
+      ]
+    }
+  end
+
+  attached_managed_policies(
+    "arn:aws:iam::550372229658:policy/EcsBasicPolicy",
+  )
+end
