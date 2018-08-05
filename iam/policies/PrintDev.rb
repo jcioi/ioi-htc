e42f0281bf4c24c86f346504bcc8cbc3bb20893d
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-08-05 10:11:27 +0900

    iam: PrintDev

---
+++ iam/policies/PrintDev.rb
@@ -0,0 +1,7 @@
+# managed_policy "PrintDev", path: path do
+#   {
+#     "Version" => "2012-10-17",
+#     "Statement" => [
+#     ]
+#   }
+# end
---
+++ iam/roles/Ec2PrintDev.rb
@@ -0,0 +1,27 @@
+role "Ec2PrintDev", :path=>"/" do
+  instance_profiles(
+    "Ec2PrintDev",
+  )
+
+  assume_role_policy_document do
+    {
+      "Version"=>"2012-10-17",
+      "Statement"=> [
+        {
+          "Effect"=>"Allow",
+          "Principal"=>{"Service"=>"ec2.amazonaws.com"},
+          "Action"=>"sts:AssumeRole",
+        },
+      ]
+    }
+  end
+
+  include_template "Ec2InstanceDefault"
+
+  attached_managed_policies(
+    "arn:aws:iam::550372229658:policy/IoiCodeDeploy",
+#    "arn:aws:iam::550372229658:policy/PrintDev",
+  )
+end
+
+instance_profile "Ec2PrintDev", :path=>"/"
