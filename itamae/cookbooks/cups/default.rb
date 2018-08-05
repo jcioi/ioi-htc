d11570089189e6f4d7131b7bd02ef50bb584ba48
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-08-05 13:08:57 +0900

    CUPS cookbook

---
+++ itamae/cookbooks/cups/default.rb
@@ -0,0 +1,28 @@
+node.reverse_merge!(
+  cups: {
+    log_level: 'warn',
+    web_interface: false,
+    listen: [
+      'localhost:631',
+    ],
+  },
+)
+
+package 'cups' do
+  action :install
+end
+
+template '/etc/cups/cupsd.conf' do
+  owner 'root'
+  group 'root'
+  mode '644'
+  notifies :restart, 'service[cups]'
+end
+
+service 'cups' do
+  action :enable
+end
+
+service 'cups-browsed' do
+  action [:disable, :stop]
+end
