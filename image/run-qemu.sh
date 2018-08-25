ec0ec3ebdfb93149c024bae8c4ef30587c421a80
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-08-25 09:37:18 +0000

    image/run-qemu.sh

---
+++ image/run-qemu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eux
+
+IMAGE=$1
+BRIDGE=$2
+MACADDR=$3
+
+if [[ $DISPLAY ]]; then
+    QEMU_DISPLAY="-display sdl -vga virtio"
+else
+    QEMU_DISPLAY=""
+fi
+
+exec qemu-system-x86_64 \
+    -machine accel=kvm -m 4G -smp 2,cores=2 -cpu host \
+    -drive if=virtio,file="${IMAGE}",format=qcow2 \
+    -netdev bridge,id=net0,br="${BRIDGE}" -device virtio-net-pci,netdev=net0,mac="${MACADDR}",bootindex=1 \
+    -bios OVMF.fd -boot menu=on \
+    $QEMU_DISPLAY
