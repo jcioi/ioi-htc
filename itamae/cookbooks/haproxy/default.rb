2b94b6face7a4b0bbdd60458558ca867e955a59a
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-08-25 17:01:35 +0900

    haproxy wants network-online for dns queries

--- itamae/cookbooks/haproxy/default.rb
+++ itamae/cookbooks/haproxy/default.rb
@@ -7,3 +7,15 @@ directory "/etc/haproxy" do
   mode  '0755'
 end
 
+directory '/etc/systemd/system/haproxy.service.d' do
+  owner 'root'
+  group 'root'
+  mode  '0755'
+end
+
+remote_file '/etc/systemd/system/haproxy.service.d/override.conf' do
+  owner 'root'
+  group 'root'
+  mode  '0644'
+  notifies :run, 'execute[systemctl daemon-reload]'
+end
