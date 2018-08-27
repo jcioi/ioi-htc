9956b6901632502c3eadf755acbf6ea39a716ee2
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-08-27 20:49:44 +0900

    print-translation-passwords

---
+++ scripts/print-translation-passwords.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require 'csv'
+require 'json'
+require 'net/http'
+require 'uri'
+
+IOIPRINT = URI(ARGV.include?('--dev') ? 'https://print-dev.ioi18.net/password' : 'https://print.ioi18.net/password')
+
+
+countries = CSV.open('./countries.csv', headers: true).read
+languages = CSV.open('./languages.csv', headers: true).read
+users = CSV.open('./users.csv', headers: true).read
+
+print_users = users.map do |user|
+  fail 'country not found' unless country = countries.find {|c| c['code'] == user['country'] }
+  fail 'language not found' unless language = languages.find {|l| l['code'] == user['language'] }
+
+  {
+    name: "#{country['name']} (#{language['name']})",
+    username: user['username'],
+    password: user['raw_password'],
+  }
+end
+
+payload = {title: "Password for Translation", users: print_users}
+
+http = Net::HTTP.new(IOIPRINT.host, IOIPRINT.port)
+http.post2(IOIPRINT.path, JSON.dump(payload),
+           'Content-Type' => 'application/json') do |resp|
+  puts resp.body
+end
