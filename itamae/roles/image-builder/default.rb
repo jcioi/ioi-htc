e2eae7823c98d221f785c4e0b9389d3d8f2ad4ee
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-08-30 12:48:17 +0900

    image-builder: ioi user in kvm group

--- itamae/roles/image-builder/default.rb
+++ itamae/roles/image-builder/default.rb
@@ -7,3 +7,7 @@ package 'pigz'
 
 include_cookbook 'packer'
 include_cookbook 'apt-cacher-ng'
+
+execute "usermod -a -G kvm ioi" do
+  only_if "! getent group kvm | cut -d: -f4 | tr , '\\n' | grep -Fxq ioi"
+end
