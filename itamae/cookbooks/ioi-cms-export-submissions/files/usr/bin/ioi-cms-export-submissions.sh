eb7d62f90ce4dc2cb712c62b949c00df7d8a9cf1
wafrelka <wafrelka@gmail.com>
2018-08-31 23:06:21 +0900

    ioi-cms-export-submissions: Fix zip directory structure

--- itamae/cookbooks/ioi-cms-export-submissions/files/usr/bin/ioi-cms-export-submissions.sh
+++ itamae/cookbooks/ioi-cms-export-submissions/files/usr/bin/ioi-cms-export-submissions.sh
@@ -3,18 +3,19 @@
 set -ue
 
 temp_dir="$(mktemp -d --tmpdir -- 'ioi-export.XXXXXXXXXX')"
-sub_dir="$temp_dir/submissions"
 date_repr="$(date +%Y-%m%d-%H%M)"
-zip_path="$temp_dir/submissions-$date_repr.zip"
+sub_dirname="submissions-$date_repr"
+zip_fname="submissions-$date_repr.zip"
 
 mkdir -p "$temp_dir"
+cd "$temp_dir"
 
 echo "exporting submissions"
-/usr/bin/ioi-cms-venv cmsExportSubmissions -y --filename "{task}/{id:05d}_{score:03.0f}_{file}{ext}" "$sub_dir"
+/usr/bin/ioi-cms-venv cmsExportSubmissions -y --filename "{task}/{id:05d}_{score:03.0f}_{file}{ext}" "$sub_dirname"
 
 echo "zipping submission files"
-zip -r "$zip_path" "$sub_dir"
+zip -r "$zip_fname" "$sub_dirname"
 
 echo "transfering zip file"
-aws s3 cp "$zip_path" "$ZIP_DESTINATION"
+aws s3 cp "$zip_fname" "$ZIP_DESTINATION"
 rm -r "$temp_dir"
