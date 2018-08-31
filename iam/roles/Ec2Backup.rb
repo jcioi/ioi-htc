cb340232b86323b0faef214cbb85f893a16c7e9a
Kasumi Hanazuki <kasumi@rollingapple.net>
2018-08-31 10:21:58 +0900

    iam: backup

---
+++ iam/roles/Ec2Backup.rb
@@ -0,0 +1,41 @@
+role "Ec2Backup", :path=>"/" do
+  instance_profiles(
+    "Ec2Backup",
+  )
+
+  assume_role_policy_document do
+    {
+      "Version"=>"2012-10-17",
+      "Statement"=> [
+        {
+          "Effect"=>"Allow",
+          "Principal"=>{"Service"=>"ec2.amazonaws.com"},
+          "Action"=>"sts:AssumeRole",
+        },
+      ]
+    }
+  end
+
+  include_template "Ec2InstanceDefault"
+
+  policy 's3' do
+    {
+      'Version' => '2012-10-17',
+      'Statement' => [
+        {
+          "Effect" => "Allow",
+          "Action" => %w(s3:ListBucket),
+          "Resource" => %w(arn:aws:s3:::ioi18-backup),
+        },
+        {
+          "Effect" => "Allow",
+          "Action" => %w(s3:PutObject s3:GetObject),
+          "Resource" => %w(arn:aws:s3:::ioi18-backup/contestant/*),
+        },
+      ],
+    }
+  end
+
+end
+
+instance_profile "Ec2Backup", :path=>"/"
