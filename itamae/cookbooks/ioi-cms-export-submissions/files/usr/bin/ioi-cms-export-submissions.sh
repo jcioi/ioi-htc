3909bb6657a4c07605ba16dda1ada3604a4abcef
wafrelka <wafrelka@gmail.com>
2018-09-03 07:20:31 +0900

    export-submissions: Surpress useless output log

--- itamae/cookbooks/ioi-cms-export-submissions/files/usr/bin/ioi-cms-export-submissions.sh
+++ itamae/cookbooks/ioi-cms-export-submissions/files/usr/bin/ioi-cms-export-submissions.sh
@@ -12,10 +12,10 @@ mkdir -p "$temp_dir"
 cd "$temp_dir"
 
 echo "exporting submissions"
-/usr/bin/ioi-cms-venv cmsExportSubmissions -y --filename "{task}/{id:05d}_{score:03.0f}_{file}{ext}" "$sub_dirname"
+/usr/bin/ioi-cms-venv cmsExportSubmissions -y --filename "{task}/{id:05d}_{score:03.0f}_{file}{ext}" "$sub_dirname" > /dev/null
 
 echo "zipping submission files"
-zip -r "$zip_fname" "$sub_dirname"
+zip -r "$zip_fname" "$sub_dirname" > /dev/null
 
 echo "transfering zip file"
 aws s3 cp "$zip_fname" "$ZIP_DESTINATION/$date_repr/"
